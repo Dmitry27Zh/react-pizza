747668dd5c541e5bb3bc069d0f8feb8c4d8a51bd
Dmitry <dmitry27zh@gmail.com>
2024-01-23 10:54:16 +0500

    Config PizzaBlock with props

--- src/App.js
+++ src/App.js
@@ -18,15 +18,8 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              <PizzaBlock />
-              <PizzaBlock />
-              <PizzaBlock />
-              <PizzaBlock />
-              <PizzaBlock />
-              <PizzaBlock />
-              <PizzaBlock />
-              <PizzaBlock />
-              <PizzaBlock />
+              <PizzaBlock title="Мексиканская" price="500" />
+              <PizzaBlock title="Чизбургер-пицца" price="300" />
             </div>
           </div>
         </div>
