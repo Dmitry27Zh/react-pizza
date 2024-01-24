a717f785303cce4aec201af544beb826ca9c481e
Dmitry <dmitry27zh@gmail.com>
2024-01-24 18:38:26 +0500

    Extract Home page component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import './App.css'
 import Header from './components/Header'
-import Categories from './components/Categories'
+import Home from './pages/Home'
 import './scss/app.scss'
-import Sort from './components/Sort'
-import { useEffect, useState } from 'react'
-import Items from './components/Items'
 
 function App() {
-  const [items, setItems] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  useEffect(() => {
-    fetch('https://65b09ea3d16d31d11bdcde5a.mockapi.io/items')
-      .then((res) => res.json())
-      .then((data) => setItems(data))
-      .catch((e) => alert(e))
-      .finally(() => setIsLoading(false))
-  }, [])
-
   return (
     <div className="App">
       <div className="wrapper">
         <Header />
         <div className="content">
           <div className="container">
-            <div className="content__top">
-              <Categories />
-              <Sort />
-            </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-              <Items isLoading={isLoading} items={items} />
-            </div>
+            <Home />
           </div>
         </div>
       </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import Categories from '../components/Categories'
+import Sort from '../components/Sort'
+import Items from '../components/Items'
+
+const Home = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+    fetch('https://65b09ea3d16d31d11bdcde5a.mockapi.io/items')
+      .then((res) => res.json())
+      .then((data) => setItems(data))
+      .catch((e) => alert(e))
+      .finally(() => setIsLoading(false))
+  }, [])
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        <Items isLoading={isLoading} items={items} />
+      </div>
+    </>
+  )
+}
+
+export default Home
