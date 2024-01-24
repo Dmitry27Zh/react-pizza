951020bcee785164e8e65c75aec6d03b881825cb
Dmitry <dmitry27zh@gmail.com>
2024-01-24 17:44:58 +0500

    Fetch items

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import Categories from './components/Categories'
 import './scss/app.scss'
 import Sort from './components/Sort'
 import PizzaBlock from './components/PizzaBlock'
-import pizzas from './assets/json/pizzas.json'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [items, setItems] = useState([])
+  useEffect(() => {
+    fetch('https://65b09ea3d16d31d11bdcde5a.mockapi.io/items')
+      .then((res) => res.json())
+      .then((data) => setItems(data))
+      .catch((e) => alert(e))
+  }, [])
+
   return (
     <div className="App">
       <div className="wrapper">
@@ -19,7 +27,7 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {pizzas.map(({ _id, ...rest }) => (
+              {items.map(({ _id, ...rest }) => (
                 <PizzaBlock key={_id} {...rest} />
               ))}
             </div>
