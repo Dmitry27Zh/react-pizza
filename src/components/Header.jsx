5403c6945b6bdf53d5d9219276643a3d0e839583
Dmitry <dmitry27zh@gmail.com>
2024-01-24 19:46:45 +0500

    Wrap logo in Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,17 @@
+import { Link } from 'react-router-dom'
 import logoIcon from '../assets/img/icons/pizza-logo.svg'
 
 const Header = () => {
   return (
     <div className="header">
       <div className="container">
-        <div className="header__logo">
+        <Link className="header__logo" to="/">
           <img width="38" src={logoIcon} alt="Pizza logo" />
           <div>
             <h1>React Pizza</h1>
             <p>самая вкусная пицца во вселенной</p>
           </div>
-        </div>
+        </Link>
         <div className="header__cart">
           <a href="/cart.html" className="button button--cart">
             <span>520 ₽</span>
