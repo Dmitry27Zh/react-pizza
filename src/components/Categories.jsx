fbd4cf7c8279c0b8a3677fb6d0f038efd37387b4
Dmitry <dmitry27zh@gmail.com>
2024-01-28 17:00:40 +0500

    Refactor categories redux usage

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,10 +1,23 @@
-const Categories = ({ items, current, onChange }) => {
+import { useDispatch, useSelector } from 'react-redux'
+import { changeCategory } from '../redux/slices/filter'
+
+const Categories = () => {
+  const { categories, category } = useSelector((state) => state.filter)
+  const dispatch = useDispatch()
+  const handleCategoryChange = (categoryId) => {
+    dispatch(changeCategory(categoryId))
+  }
+
   return (
     <div className="categories">
       <ul>
-        {items.map((item) => (
-          <li key={item._id} className={item._id === current ? 'active' : ''} onClick={() => onChange(item._id)}>
-            {item.title}
+        {categories.map((current) => (
+          <li
+            key={current._id}
+            className={current._id === category ? 'active' : ''}
+            onClick={() => handleCategoryChange(current._id)}
+          >
+            {current.title}
           </li>
         ))}
       </ul>
