ed8d5f293695d113e5af5ebb3dd8e42c45b7fd5a
Dmitry <dmitry27zh@gmail.com>
2024-01-30 17:48:19 +0500

    Start cart store

---
+++ src/redux/slices/cart/index.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  items: [],
+}
+
+const slice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      state.items.push(action.payload)
+    },
+    removeItem: (state, action) => {
+      state.items = state.items.filter((item) => item._id !== action.payload)
+    },
+    clearItems: (state) => {
+      state.items = []
+    },
+  },
+})
+
+export const { addItem, removeItem, clearItems } = slice.actions
+
+export default slice.reducer
