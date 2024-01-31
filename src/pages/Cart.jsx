d2862825d06060072f50503287b97de754191b3b
Dmitry <dmitry27zh@gmail.com>
2024-01-31 10:11:32 +0500

    Extract CartEmpty component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,10 @@
+import CartEmpty from '../components/CartEmpty'
 import CartItems from '../components/CartItems'
 
 const Cart = () => {
-  return (
-    <div className="container container--cart">
-      <CartItems />
-    </div>
-  )
+  const isEmpty = true
+
+  return <div className="container container--cart">{isEmpty ? <CartEmpty /> : <CartItems />}</div>
 }
 
 export default Cart
