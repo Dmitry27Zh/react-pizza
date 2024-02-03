a98ad06ac60a7eb0bed02eae9820505e09892ce0
Dmitry <dmitry27zh@gmail.com>
2024-02-03 12:13:52 +0500

    Create increment reducer in cartSlice

--- src/redux/slices/cart/index.js
+++ src/redux/slices/cart/index.js
@@ -15,14 +15,19 @@ const slice = createSlice({
       const isExist = key in state.items
 
       if (isExist) {
-        let { count, price, total } = state.items[key]
-        count++
-        total += price
-        state.items[key] = { count, price, total }
+        slice.caseReducers.increment(state, action)
       } else {
         state.items[key] = { count: 1, price, total: price }
+        state.count++
+        state.total += price
       }
-
+    },
+    increment: (state, action) => {
+      const { key } = action.payload
+      let { count, price, total } = state.items[key]
+      count++
+      total += price
+      state.items[key] = { count, price, total }
       state.count++
       state.total += price
     },
